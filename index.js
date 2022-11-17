7acb3faa2fe7efa30feb467b5bcd41d8642da4ee
dcaaz <danicreaazevedo@gmail.com>
2022-11-17 18:42:44 -0300

    post cadastro

--- index.js
+++ index.js
@@ -4,22 +4,61 @@ import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
 import joi from "joi";
 
+const cadastroSchema = joi.object({
+    nome: joi.string().required().min(3),
+    email: joi.string().required().min(3),
+    senha: joi.string().required().min(3),
+});
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json()); //receber req do cliente no formato json
 
-const mongoClient = new MongoClient(process.env.MONGO_URI); //porta do mongo
+const mongoClient = new MongoClient("mongodb://localhost:27017"); //porta do mongo
 
 try {
     await mongoClient.connect();
     console.log("MongoDB conectado!");
 } catch (err) {
     console.log("err mongoDB", err);
-}
+};
 
 const db = mongoClient.db("myWallet");
+
+const users = db.collection("usuarios");
+
+app.post("/cadastro", async (req, res) => {
+
+    const { nome, email, senha } = req.body;
+
+    const validation = cadastroSchema.validate(
+        { nome, email, senha },
+        { abortEarly: false }
+    );
+
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+        return res.status(422).send(errors);
+    };
+
+    const novoPerfil =
+    {
+        nome,
+        email,
+        senha
+    };
+
+    try {
+        await users.insertOne(novoPerfil); // inserindo no mongo
+        res.sendStatus(201);
+    } catch (err) { //se der errado
+        res.sendStatus(500);
+    };
+
+});
+
 
 
 app.listen(5000, () => {
